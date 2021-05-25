2bbd55b0cdb696eda242884d9358adc15b5184ba
southoz <adriangrant@iinet.net.au>
2021-05-25 20:44:30 +1000

    Update def.h

--- src/def.h
+++ src/def.h
@@ -8,15 +8,15 @@
 #define SCREEN_WIDTH          854
 // #define SCREEN_HEIGHT         240
 #define SCREEN_HEIGHT         480
-#define SCALE_WIDTH           480  
-#define SCALE_HEIGHT          320
+#define SCALE_WIDTH           854 
+#define SCALE_HEIGHT          3480
 
 #define SCREEN_BPP            16
 #define SURFACE_FLAGS         SDL_SWSURFACE
 #define MS_PER_FRAME          33
 // Panel
-#define LINE_HEIGHT           15
-#define NB_VISIBLE_LINES      14
+#define LINE_HEIGHT           20
+#define NB_VISIBLE_LINES      19
 #define Y_LIST                17
 #define Y_HEADER              3
 #define Y_FOOTER              228
